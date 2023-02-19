98166e091700efc2b07a5efae2356ed675cdda42
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-02-18 23:36:06 -0300

    exe3

---
+++ vaiaveis_expressoes/exe2.rb
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número real e o imprima.
+
+print "Digite um numero real: ";
+
+numero = Float(gets());
+
+puts "#{numero}";
---
+++ vaiaveis_expressoes/exe3.rb
@@ -0,0 +1,21 @@
+=begin
+
+Peça ao usuário para digitar três valores inteiros e imprima a soma deles.
+
+=end
+
+puts "valor1: ";
+
+valor1 = Integer(gets());
+
+puts "Valor2: ";
+
+valor2 = Integer(gets());
+
+puts "Valor3: ";
+
+valor3 = Integer(gets());
+
+soma = valor1 + valor2 + valor3;
+
+puts "A soma dos valores: #{soma}";
