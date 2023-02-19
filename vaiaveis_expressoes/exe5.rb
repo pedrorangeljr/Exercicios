c3c8dd36cd74f37da123aa3fba455f6442d1967a
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2023-02-19 10:46:27 -0300

    exe5

---
+++ vaiaveis_expressoes/exe5.rb
@@ -0,0 +1,10 @@
+
+# Leia um número real e imprima a quinta parte deste número.
+
+print "Digite um numero real: ";
+
+numero = Float(gets());
+
+quinta_parte = numero / 5;
+
+puts "quinta parte deo número é: #{quinta_parte}";
